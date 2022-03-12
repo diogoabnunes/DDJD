f54fa9366575908bed8ad5c44bd326dda377c99c
Diogo Santos <diogofilipe1_santos@hotmail.com>
2022-03-12 18:24:48 +0000

    Added cool down to tiger shots

--- TEUP to Survive/Assets/Player/PlayerMovement.cs
+++ TEUP to Survive/Assets/Player/PlayerMovement.cs
@@ -10,11 +10,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private GameObject tigerShot;
 
     private bool jump = false;
+    private float lastShot;
+    private float coolDownShot;
 
     // Start is called before the first frame update
     void Start()
     {
        unstoppable = false; 
+       lastShot = -1f;
+       coolDownShot = 0.5f;
     }
 
     // Update is called once per frame
@@ -25,9 +29,9 @@ public class PlayerMovement : MonoBehaviour
             jump = true;
         }
 
-        if (Input.GetKey(KeyCode.Return))
+        if (Input.GetKey(KeyCode.Return) && CanShot())
         {
-            Instantiate(tigerShot, transform.position, transform.rotation);
+            Shot();
         }
     }
 
@@ -36,5 +40,18 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(Vector2.up * 7.5f * speed);
             jump = false;
         }
+    }
+
+    private bool CanShot() {
+        if (lastShot == -1 || (Time.time - lastShot) > coolDownShot) {
+            lastShot = Time.time;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Shot() {
+        Instantiate(tigerShot, transform.position, transform.rotation);
     }
 }
