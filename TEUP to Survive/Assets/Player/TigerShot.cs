32c31dc20f1ce8a16402958383ac4a29ed60eb0a
Diogo Nunes <up201808546@fe.up.pt>
2022-03-15 14:39:32 +0000

    now score increases when player hits enemy (in 50points)

--- TEUP to Survive/Assets/Player/TigerShot.cs
+++ TEUP to Survive/Assets/Player/TigerShot.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class TigerShot : MonoBehaviour
 {
-
     private Rigidbody2D rb;
     private GameSettings gameSettings;
+
+    private ScoreManager scoreCanvas;
+    private float pointsPerEnemy = 50f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         gameSettings = FindObjectsOfType<GameSettings>()[0];
+        scoreCanvas = FindObjectsOfType<ScoreManager>()[0];
 
         rb.velocity = new Vector2(-gameSettings.scrollSpeed, 0);
     }
@@ -24,5 +27,12 @@ public class TigerShot : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.tag == "RightBorder") Destroy(this.gameObject);
+
+        else if (collision.tag == "Enemy") {
+            Destroy(this.gameObject);
+            scoreCanvas.score += (int)pointsPerEnemy;
+            scoreCanvas.scoreText.text = (int)scoreCanvas.score + "";
+        }
+    
     }
 }
