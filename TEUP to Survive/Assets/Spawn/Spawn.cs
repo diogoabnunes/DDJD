d8cc7160129ae1d86d8a93a5159777875bd41144
Diogo Nunes <up201808546@fe.up.pt>
2022-03-16 09:03:52 +0000

    spawn from main

---
+++ TEUP to Survive/Assets/Spawn/Spawn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject[] objects;
+    [SerializeField] private float[] objectsRate;
+
+    private float nextSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
