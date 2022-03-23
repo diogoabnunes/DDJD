438c7fe1ba69cd4c7801d4e2b5689ad47994d709
Marcelo Reis <up201809566@fe.up.pt>
2022-03-23 14:50:44 +0000

    Refactor obstacles and changed some scene objects.

--- TEUP to Survive/Assets/Obstacles/Obstacle.cs
+++ TEUP to Survive/Assets/Obstacles/Obstacle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour
+public class Obstacle : MonoBehaviour
 {
     private GameObject player;
 
