4c9cc426133de123b7bed4b6534be5faece16c7b
Diogo Nunes <up201808546@fe.up.pt>
2022-03-26 00:38:37 +0000

    simplifying

--- TEUP to Survive/Assets/Obstacles/Obstacle.cs
+++ TEUP to Survive/Assets/Obstacles/Obstacle.cs
@@ -5,17 +5,13 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     private GameObject player;
-    private BirdSound birdSound;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        birdSound = FindObjectsOfType<BirdSound>()[0];
-        birdSound.On();
     }
 
     private void OnTriggerEnter2D(Collider2D collision){
-        birdSound.Off();
         if(collision.tag == "LeftBorder"){
             Destroy(this.gameObject);
         }else if (collision.tag == "Player" && (!player.GetComponent<PlayerPowerUps>().IsUnstoppable())){
