047d351b3ad34f2223c164d809cf3ff109764309
Diogo Santos <diogofilipe1_santos@hotmail.com>
2022-03-29 08:58:09 +0100

    Added last enemie

--- TEUP to Survive/Assets/GameSettings/GameSettings.cs
+++ TEUP to Survive/Assets/GameSettings/GameSettings.cs
@@ -7,11 +7,6 @@ public class GameSettings : MonoBehaviour
     public float scrollSpeed; // current game speed
     public float speedIncrement; // game increment after some time
 
-    void Start() {
-        scrollSpeed = -2f;
-        speedIncrement = 0.0001f;
-    }
-
     void Update() {
         // MAYBE JUST UPDATE AFTER EVERY 10 SECONDS ???
         scrollSpeed -= speedIncrement;
