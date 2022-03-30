e4536f39281cc6fe103f1ddac89f3c53afba2645
Marcelo Reis <up201809566@fe.up.pt>
2022-03-30 19:09:59 +0100

    Finished birds animation.

--- TEUP to Survive/Assets/Obstacles/FlyingObstacle/BirdAnimation.cs
+++ TEUP to Survive/Assets/Obstacles/FlyingObstacle/BirdAnimation.cs
@@ -8,7 +8,11 @@ public class BirdAnimation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Animator>().SetInteger("AnimationSelected",Random.Range(0,numOfAnimations));
+        Animator anim = GetComponent<Animator>();
+        anim.SetInteger("AnimationSelected",Random.Range(0,numOfAnimations)); // Select Random Animation
+
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+        anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f)); // Play animation at random frame
     }
 
 }
