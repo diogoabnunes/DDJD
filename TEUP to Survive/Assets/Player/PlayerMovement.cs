daa12f995fedc1b3ecd9ac246d4c6db74d63a2bd
Marcelo Reis <up201809566@fe.up.pt>
2022-04-02 15:41:15 +0100

    Added walk animation to tiger and tiger with continuous fire.

--- TEUP to Survive/Assets/Player/PlayerMovement.cs
+++ TEUP to Survive/Assets/Player/PlayerMovement.cs
@@ -8,10 +8,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
 
     private bool jump;
+    private Animator animator;
+    private Animator animatorContinuousFire;
 
     void Start()
     {
         jump = false; 
+        animator = transform.GetChild(1).GetComponent<Animator>();
+        animatorContinuousFire = transform.GetChild(2).GetComponent<Animator>();
     }
 
     void Update()
@@ -21,8 +25,22 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate() {
         if (jump) {
+            animator.SetBool("IsMoving", false);
+            animator.SetBool("JetPackOn", true);
+            animatorContinuousFire.SetBool("IsMoving", false);
+            animatorContinuousFire.SetBool("JetPackOn", true);
             rb.AddForce(Vector2.up * 7.5f * speed);
             jump = false;
+        }else{
+            animator.SetBool("JetPackOn", false);
+            animatorContinuousFire.SetBool("JetPackOn", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "BottomBorder") {
+            animator.SetBool("IsMoving", true);
+            animatorContinuousFire.SetBool("IsMoving", true);
         }
     }
 }
