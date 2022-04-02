3e15e9907ef12e32347c4168579ee5cbd04e4091
Marcelo Reis <up201809566@fe.up.pt>
2022-04-02 13:22:50 +0100

    Vary background.

--- TEUP to Survive/Assets/Background/BackgroundScroller.cs
+++ TEUP to Survive/Assets/Background/BackgroundScroller.cs
@@ -6,15 +6,18 @@ public class BackgroundScroller : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D backgroundCollider;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Sprite[] backgrounds;
 
     private float width;
     private GameSettings gameSettings;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         backgroundCollider = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         gameSettings = FindObjectsOfType<GameSettings>()[0];
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         width = backgroundCollider.size.x * transform.localScale.x;
         backgroundCollider.enabled = false;
@@ -25,6 +28,8 @@ public class BackgroundScroller : MonoBehaviour
     void FixedUpdate()
     {
         if (transform.position.x < (-width * 1.5)){
+            int index = Random.Range(0,backgrounds.Length);
+            spriteRenderer.sprite = backgrounds[index];
             Vector2 resetPosition = new Vector2(width * 3f, 0);
             transform.position = (Vector2)transform.position + resetPosition;
         }
